17bb25eb16d3a158bb6437dae11d3388f2660862
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2022-05-27 16:38:00 -0300

    Update atv08.c

--- estrutura sequencial/atv08.c
+++ estrutura sequencial/atv08.c
@@ -1,13 +1,13 @@
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main(){
   float temp;
+  
   printf("Adicione a temperatura em graus F:\n");
   scanf("%f",&temp);
+  
   float calc=5*((temp-32)/9);
   printf("A temperatura em celsius será:%.2f",calc);
 
   return 0;
-}*/
-//Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
-//C = 5 * ((F-32) / 9).
+}
