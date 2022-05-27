7b927ea2164349abdc8fbe7e63d8ab80d68c72c5
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2022-05-27 15:58:21 -0300

    atualizacao

---
+++ estrutura sequencial/atv01.c
@@ -0,0 +1,14 @@
+/*#include <stdio.h>
+
+int main(){ //declarar variável
+
+  int numero; 
+  //declare a função
+  printf("Digite um número:\n"); 
+  scanf("%i", &numero);
+
+  //aqui vai informar o número digitado
+  printf("O número informado foi %i", numero);
+
+  return 0;
+}*/
